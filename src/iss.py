ac6a6c401d53418f7bec2765026947f0a2bfcf11
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2018-05-17 17:57:03 +0100

    reading the matlab file

--- src/iss.py
+++ src/iss.py
@@ -1,9 +1,17 @@
 import json
 import os
+import glob
 import src.utils as utils
+import logging
+
+
+logger = logging.getLogger()
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(message)s"
+    )
 
 my_path = os.path.abspath(os.path.dirname(__file__))
-from pprint import pprint
 
 
 def populate():
@@ -11,7 +19,18 @@ def populate():
     with open(config_file) as f:
         out = json.load(f)
 
-    out["TileFiles"] = utils.parse_mat_array("TileFiles")
+    mat_folder = os.path.join(my_path, "../data/mat")
+    for file in os.listdir(mat_folder):
+        if file.endswith(".mat"):
+            path_str = os.path.join(mat_folder, file)
+            key = os.path.splitext(file)[0]
+            temp = utils.loadmat(path_str)
+            msg = " Populating %s" % key
+            logger.info(msg)
+            out[key] = temp[key]
 
+    # utils.loadmat(filename)
+    # out["TileFiles"] = utils.parse_mat_array("TileFiles")
+    # out["cSpotColors"] = utils.parse_mat_array("cSpotColors")
 
     return out
