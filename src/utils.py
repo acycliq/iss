70582aac616206a1a38fbe19ab65b066d883d322
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2018-05-17 17:57:35 +0100

    added a method to read Matlab mat files

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy.io
+import scipy.io as spio
 import os
 
 def parse_mat_array(name):
@@ -11,13 +11,51 @@ def parse_mat_array(name):
     rel_path = "../data/" + name + ".mat"
     path_str = os.path.join(proj_path, rel_path)
     print(path_str)
-    mat = scipy.io.loadmat(path_str)
+    mat = spio.loadmat(path_str)
     x = mat[name]
     out = np.nan * np.ones(x.shape, dtype=object)
 
     for i in range(out.shape[0]):
         for j in range(out.shape[1]):
             for k in range(out.shape[2]):
-                out[i, j, k] = x[i, j, k][0]
+                out[i, j, k] = x[i, j, k]
 
     return out
+
+
+def loadmat(filename):
+    '''
+    this function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+
+    from: `StackOverflow <http://stackoverflow.com/questions/7008608/scipy-io-loadmat-nested-structures-i-e-dictionaries>`_
+    '''
+    data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+
+def _check_keys(dict):
+    '''
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    '''
+    for key in dict:
+        if isinstance(dict[key], spio.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict
+
+
+def _todict(matobj):
+    '''
+    A recursive function which constructs from matobjects nested dictionaries
+    '''
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
