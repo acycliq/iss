6685095ba95aeeb1b176c08fbb4dc14532d8c619
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2018-05-21 16:37:34 +0100

    Developing the quality_threshold method. Needs to be checked!!

--- src/iss.py
+++ src/iss.py
@@ -28,11 +28,26 @@ class Iss:
         mat = utils.loadmat(self.CellMapFile)
         self.cell_map = mat["CellMap"]
 
-    def clean_data(self):
+    def filter_data(self):
         exclude_genes = ['Vsnl1', 'Atp1b1', 'Slc24a2', 'Tmsb10', 'Calm2', 'Gap43', 'Fxyd6']
-        all_gene_names = self.GeneNames[self.SpotCodeNo]
+        all_gene_names = self.GeneNames[self.SpotCodeNo-1] # -1 is needed because Matlab is 1-based
         cond_1 = ~np.isin(all_gene_names, exclude_genes)
         cond_2 = utils.inpolygon(self.SpotGlobalYX[:, 0], self.SpotGlobalYX[:, 1], self.CellCallRegionYX[:, 0], self.CellCallRegionYX[:, 1])
+        cond_3 = self.quality_threshold()
 
     def call_cells(self):
         print("todo")
+
+    def quality_threshold(self):
+        qual_ok = self.SpotCombi & (self.SpotScore > self.CombiQualThresh) & (self.SpotIntensity > self.CombiIntensityThresh);
+
+        anchors_ok = np.ones(qual_ok.shape)
+        is_greater = self.cAnchorIntensities > self.DetectionThresh
+        tot = is_greater.sum(axis=1)
+        idx = tot > self.CombiAnchorsReq
+
+        anchors_ok[self.SpotCombi>0] = idx
+
+        qual_ok = qual_ok & anchors_ok
+
+
