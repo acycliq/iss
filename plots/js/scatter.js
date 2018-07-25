451b19ea2a09864be8c029183a059efa557732d5
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2018-07-25 15:56:43 +0100

    some more work on the datatable

--- plots/js/scatter.js
+++ plots/js/scatter.js
@@ -201,36 +201,44 @@ function initChart(data) {
                 .replace(/,/g, '<br>'));
 
             var mydata = [];
+            var mydata2 = [];
 //            mydata.push({
 //                Num: d.Cell_Num,
 //                X: d.x,
 //                Y: d.y,
 //            });
-//            
-            var n = d3.max([d.CellGeneCount.length, d.Cell_Num.length, d.ClassName.length, d.Genenames.length, d.Prob.length]);
+//          
+            var str =   "<strong>Cell Num: </strong>" + d.Cell_Num + 
+                        ",  (<strong>x, y</strong>): (" + d.x.toFixed(2) + ", " + d.y.toFixed(2)+")" ;
+            document.getElementById('dtTitle').innerHTML = str;
+            var n = d3.max([d.CellGeneCount.length, d.Genenames.length]);
             for (i = 0; i < n; i++) {
                 mydata.push({
-//                    Num: d.Cell_Num,
-//                    X: d.x,
-//                    Y: d.y,
-//                    "CellGeneCount": d[0].CellGeneCount,
-//                    "Cell_Num": d[0].Cell_Num,
-                    "CellGeneCount": (d.CellGeneCount[i] === undefined)? "": d.CellGeneCount[i],
-                    "ClassName": (d.ClassName[i] === undefined)? "": d.ClassName[i],
                     "Genenames": (d.Genenames[i] === undefined)? "": d.Genenames[i],
+                    "CellGeneCount": (d.CellGeneCount[i] === undefined)? "": d.CellGeneCount[i],
+                })
+            }
+            
+            var n = d3.max([d.ClassName.length, d.Prob.length]);
+            for (i = 0; i < n; i++) {
+                mydata2.push({
+                    "ClassName": (d.ClassName[i] === undefined)? "": d.ClassName[i],
                     "Prob": (d.Prob[i] === undefined)? "": d.Prob[i],
                 })
             }
 
 
+
             
-            
-            if ( $.fn.dataTable.isDataTable( '#dt' ) ) {
-                table = $('#dt').DataTable();
+            // check if a there is a reference to a datatable.
+            // If yes, refresh with the new data
+            // Otherwise create and populate a datatable
+            if ( $.fn.dataTable.isDataTable( '#dtTable' ) ) {
+                table = $('#dtTable').DataTable();
                 table.clear().rows.add(mydata).draw();
             }
             else {
-                    table = $('#dt').DataTable({
+                    table = $('#dtTable').DataTable({
                         //bFilter: false,
                         "lengthChange": false,
                         searching: false,
@@ -238,18 +246,46 @@ function initChart(data) {
                         //"scrollCollapse": true,
                         "paging":         true,
                         //dom: 't',
+                        
                         "data": mydata,
                         "columns": [
                             {title: "Gene Names", data: "Genenames"},
                             {title: "Cell Gene Count", data: "CellGeneCount"},
+		                  ],
+                    });
+                
+            }
+
+            
+            if ( $.fn.dataTable.isDataTable( '#dtTable2' ) ) {
+                table2 = $('#dtTable2').DataTable();
+                table2.clear().rows.add(mydata2).draw();
+            }
+            else {
+                    table2 = $('#dtTable2').DataTable({
+                        //bFilter: false,
+                        "lengthChange": false,
+                        searching: false,
+                        //"scrollY":        "200px",
+                        //"scrollCollapse": true,
+                        "paging":         true,
+                        //dom: 't',
+                        "data": mydata2,
+                        "columns": [
                             {title: "Class Name", data: "ClassName"},
                             {title: "Prob", data: "Prob"},
 		              ]
                     });
             }
 
-
-
+            // Sort by column 1 and then re-draw
+            table
+                .order( [ 1, 'desc' ] )
+                .draw();
+            
+            table2
+                .order( [ 1, 'desc' ] )
+                .draw();
 
 
 
