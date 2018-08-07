758a23882d91e1193e506a2bd37493917ea3f95f
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2018-08-07 11:14:04 +0100

    i messed up my branches!

--- plots/js/scatterPlot_zoom.js
+++ plots/js/scatterPlot_zoom.js
@@ -9,30 +9,36 @@ function scatterPlot(data) {
     //var width = 600;
     //var height = 400;
 
-    // padding around the chart where axes will go
-    var padding = {
+    // margin around the chart where axes will go
+    var margin = {
             top: 10,
             left: 50,
             bottom: 30,
             right: 0
         },
-        width = 920 - padding.left - padding.right,
-        height = 480 - padding.top - padding.bottom;
+        width = 920 - margin.left - margin.right,
+        height = 480 - margin.top - margin.bottom;
 
     // inner chart dimensions, where the dots are plotted
-    var plotAreaWidth = width - padding.left - padding.right;
-    var plotAreaHeight = height - padding.top - padding.bottom;
+    var plotAreaWidth = width - margin.left - margin.right;
+    var plotAreaHeight = height - margin.top - margin.bottom;
 
     // radius of points in the scatterplot
     var pointRadius = 2;
 
     var extent = {
-        x: d3.extent(data, function (d) {
-            return d.x
-        }),
-        y: d3.extent(data, function (d) {
-            return d.y
-        })
+        x: d3.extent(data, function (d) {return d.x}),
+        y: d3.extent(data, function (d) {return d.y})
+    };
+
+    var scale = {
+        x: d3.scaleLinear().range([0, plotAreaWidth]),
+        y: d3.scaleLinear().range([plotAreaHeight, 0]),
+    };
+
+    var axis = {
+        x: d3.axisBottom(scale.x).ticks(xTicks).tickSizeOuter(0),
+        y: d3.axisLeft(scale.y).ticks(yTicks).tickSizeOuter(0),
     };
 
     // initialize scales
@@ -43,37 +49,30 @@ function scatterPlot(data) {
     // select the root container where the chart will be added
     var container = d3.select('#scatter-plot');
 
+    var zoom = d3.zoom()
+        .scaleExtent([1, 5])
+        .on("zoom", zoomed);
+
     // initialize main SVG
     var svg = container.select('svg')
-        .attr("width", width + padding.left + padding.right)
-        .attr("height", height + padding.top + padding.bottom);
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .call(zoom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // the main <g> where all the chart content goes inside
-    var g = svg.append('g')
-        .attr('transform', `translate(${padding.left} ${padding.top})`);
+//    var g = svg.append('g')
+//        .attr('transform', `translate(${margin.left} ${margin.top})`);
 
     // add in axis groups
-    var xAxisG = g.append('g').classed('x-axis', true)
+    var xAxisG = svg.append('g').classed('x-axis', true)
         .attr('transform', `translate(0 ${plotAreaHeight + pointRadius})`);
 
-    // x-axis label
-    g.append('text')
-        .attr('transform', `translate(${plotAreaWidth / 2} ${plotAreaHeight + (padding.bottom)})`)
-        .attr('dy', +10) // adjust distance from the bottom edge
-        .attr('class', 'axis-label')
-        .attr('text-anchor', 'middle')
-        .text('X Axis');
 
-    var yAxisG = g.append('g').classed('y-axis', true)
+    var yAxisG = svg.append('g').classed('y-axis', true)
         .attr('transform', `translate(${-pointRadius} 0)`);
 
-    // y-axis label
-    g.append('text')
-        .attr('transform', `rotate(270) translate(${-plotAreaHeight / 2} ${-padding.left})`)
-        .attr('dy', 12) // adjust distance from the left edge
-        .attr('class', 'axis-label')
-        .attr('text-anchor', 'middle')
-        .text('Y Axis');
 
     // set up axis generating functions
     var xTicks = Math.round(plotAreaWidth / 50);
@@ -93,7 +92,7 @@ function scatterPlot(data) {
 
 
     // add in circles
-    var circles = g.append('g').attr('class', 'circles');
+    var circles = svg.append('g').attr('class', 'circles');
     var binding = circles.selectAll('.data-point').data(data, d => d.id);
     binding.enter().append('circle')
         .classed('data-point', true)
@@ -107,6 +106,23 @@ function scatterPlot(data) {
         .attr("cy", d => xScale(d.y))
         .attr("r", 2)
         .style("opacity", 1);
+
+    function updateScales(data, scale){
+        scale.x.domain(extent.x),
+        scale.y.domain(extent.y)
+    }
+
+    function zoomed() {
+//        d3.event.transform.y = 0;
+//        d3.event.transform.x = Math.min(d3.event.transform.x, 5);
+//        d3.event.transform.x = Math.max(d3.event.transform.x, (1 - d3.event.transform.k) * width);
+
+        // update: rescale x axis
+        renderXAxis.call(axis.x.scale(d3.event.transform.rescaleX(scale.x)));
+
+        // Make sure that only the x axis is zoomed
+        heatDotsGroup.attr("transform", d3.event.transform.toString().replace(/scale\((.*?)\)/, "scale($1, 1)"));
+    }
 
 
     // ----------------------------------------------------
@@ -125,7 +141,7 @@ function scatterPlot(data) {
 
 
     // add a circle for indicating the highlighted point
-    g.append('circle')
+    svg.append('circle')
         .attr('class', 'highlight-circle')
         .attr('r', pointRadius + 2) // slightly larger than our points
         .style('fill', 'none')
@@ -136,8 +152,6 @@ function scatterPlot(data) {
         // no point to highlight - hide the circle and clear the text
         if (!d) {
             d3.select('.highlight-circle').style('display', 'none');
-            highlightOutput.text('');
-            highlightOutput2.text('');
 
             // otherwise, show the highlight circle at the correct position
         } else {
@@ -170,16 +184,17 @@ function scatterPlot(data) {
                 labels: site.data.ClassName[i]
             })
         }
+        console.log(data.length)
     }
 
     // add the overlay on top of everything to take the mouse events
-    g.append('rect')
+    svg.append('rect')
         .attr('class', 'overlay')
         .attr('width', plotAreaWidth)
         .attr('height', plotAreaHeight)
         .style('fill', 'red')
         .style('opacity', 0)
-        .on('click', mouseMoveHandler)
+        .on('mousemove', mouseMoveHandler)
         .on('mouseleave', () => {
             // hide the highlight circle when the mouse leaves the chart
             //highlight(null);
