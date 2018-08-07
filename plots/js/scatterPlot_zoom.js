0b60bbac71e9394e83e92a24d6b93603f57d35b8
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2018-08-07 15:44:48 +0100

    code clean-up

--- plots/js/scatterPlot_zoom.js
+++ plots/js/scatterPlot_zoom.js
@@ -1,15 +1,5 @@
 function scatterPlot(data) {
 
-
-    // ----------------------------------------------------
-    // Build a basic scatterplot
-    // ----------------------------------------------------
-
-    // outer svg dimensions
-    //var width = 600;
-    //var height = 400;
-
-    // margin around the chart where axes will go
     var margin = {
             top: 10,
             left: 50,
@@ -28,7 +18,7 @@ function scatterPlot(data) {
 
     var extent = {
         x: d3.extent(data, function (d) {return d.x}),
-        y: d3.extent(data, function (d) {return d.y})
+        y: d3.extent(data, function (d) {return d.y}),
     };
 
     var scale = {
@@ -40,10 +30,8 @@ function scatterPlot(data) {
         x: d3.axisBottom(scale.x).ticks(xTicks).tickSizeOuter(0),
         y: d3.axisLeft(scale.y).ticks(yTicks).tickSizeOuter(0),
     };
+    
 
-    // initialize scales
-    var xScale = d3.scaleLinear().domain([extent.x[0] - 100, extent.x[1] + 100]).range([0, plotAreaWidth]).nice();
-    var yScale = d3.scaleLinear().domain([extent.y[0] - 100, extent.y[1] + 100]).range([plotAreaHeight, 0]).nice();
     var colorScale = d3.scaleLinear().domain([0, 1]).range(['#06a', '#06a']);
 
     // select the root container where the chart will be added
@@ -60,56 +48,24 @@ function scatterPlot(data) {
         .call(zoom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    //Create X axis
+    renderXAxis = svg.append("g")
+        .attr("class", "x axis")
 
-    // the main <g> where all the chart content goes inside
-//    var g = svg.append('g')
-//        .attr('transform', `translate(${margin.left} ${margin.top})`);
-
-    // add in axis groups
-    var xAxisG = svg.append('g').classed('x-axis', true)
-        .attr('transform', `translate(0 ${plotAreaHeight + pointRadius})`);
-
-
-    var yAxisG = svg.append('g').classed('y-axis', true)
-        .attr('transform', `translate(${-pointRadius} 0)`);
+    //Create Y axis
+    var renderYAxis = svg.append("g")
+        .attr("class", "y axis")
 
 
     // set up axis generating functions
     var xTicks = Math.round(plotAreaWidth / 50);
     var yTicks = Math.round(plotAreaHeight / 50);
 
-    var xAxis = d3.axisBottom(xScale)
-        .ticks(xTicks)
-        .tickSizeOuter(0);
-
-    var yAxis = d3.axisLeft(yScale)
-        .ticks(yTicks)
-        .tickSizeOuter(0);
-
-    // draw the axes
-    yAxisG.call(yAxis);
-    xAxisG.call(xAxis);
-
-
-    // add in circles
-    var circles = svg.append('g').attr('class', 'circles');
-    var binding = circles.selectAll('.data-point').data(data, d => d.id);
-    binding.enter().append('circle')
-        .classed('data-point', true)
-        .attr('r', pointRadius)
-        .attr('cx', d => xScale(d.x))
-        .attr('cy', d => yScale(d.y))
-        .attr('fill', d => colorScale(d.y));
-
-    binding.transition().duration(1000)
-        .attr("cx", d => xScale(d.x))
-        .attr("cy", d => xScale(d.y))
-        .attr("r", 2)
-        .style("opacity", 1);
-
+    
     function updateScales(data, scale){
-        scale.x.domain(extent.x),
-        scale.y.domain(extent.y)
+        scale.x.domain([extent.x[0] - 100, extent.x[1] + 100]).nice(),
+        scale.y.domain([extent.y[0] - 100, extent.y[1] + 100]).nice()
     }
 
     function zoomed() {
@@ -124,6 +80,38 @@ function scatterPlot(data) {
         heatDotsGroup.attr("transform", d3.event.transform.toString().replace(/scale\((.*?)\)/, "scale($1, 1)"));
     }
 
+    renderPlot(data);
+    
+    function renderPlot(data){
+        updateScales(data, scale);
+        
+        svg.select('.y.axis')
+            .attr("transform", "translate(" + -pointRadius + " 0)" )
+            .call(axis.y);
+        
+        var h = plotAreaHeight + pointRadius;
+        svg.select('.x.axis')
+            .attr("transform", "translate(0, " + h + ")")
+            .call(axis.x);
+        
+        // add in circles
+        var circles = svg.append('g').attr('class', 'circles');
+        var binding = circles.selectAll('.data-point').data(data, d => d.id);
+        binding.enter().append('circle')
+            .classed('data-point', true)
+            .attr('r', pointRadius)
+            .attr('cx', d => scale.x(d.x))
+            .attr('cy', d => scale.y(d.y))
+            .attr('fill', d => colorScale(d.y));
+
+        binding.transition().duration(1000)
+            .attr("cx", d => scale.x(d.x))
+            .attr("cy", d => scale.x(d.y)) // This is meant to be scale.y probably
+            .attr("r", 2)
+            .style("opacity", 1);
+        
+        
+    }
 
     // ----------------------------------------------------
     // Add in Voronoi interaction
@@ -132,8 +120,8 @@ function scatterPlot(data) {
     // add in interaction via voronoi
     // create a voronoi diagram based on the data and the scales
     var voronoiDiagram = d3.voronoi()
-        .x(d => xScale(d.x))
-        .y(d => yScale(d.y))
+        .x(d => scale.x(d.x))
+        .y(d => scale.y(d.y))
         .size([plotAreaWidth, plotAreaHeight])(data);
 
     // limit how far away the mouse can be from finding a voronoi site
@@ -158,8 +146,8 @@ function scatterPlot(data) {
             d3.select('.highlight-circle')
                 .style('display', '')
                 .style('stroke', colorScale(d.y))
-                .attr('cx', xScale(d.x))
-                .attr('cy', yScale(d.y));
+                .attr('cx', scale.x(d.x))
+                .attr('cy', scale.y(d.y));
         }
     }
 
